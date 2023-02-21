b8d3518e784108c3f05c109889c31f973e055397
Hamza_OK <hamza.okhadir2018@gmail.com>
2023-02-21 09:14:31 -0800

    Task Ne: 3

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,8 @@
-int _putchar(char c);
+int _putchar(char);
 /* declare _putchar function to avoid the  [-Werror=implicit-function-declaration]*/
 void print_alphabet(void);
 /* declare print_alphabet function to avoid the  [-Werror=implicit-function-declaration]*/
 void print_alphabet_x10(void);
 /* declare print_alphabet_x10 function to avoid the  [-Werror=implicit-function-declaration]*/
-
+int _islower(int);
+/* declare _islower function to avoid the  [-Werror=implicit-function-declaration]*/
