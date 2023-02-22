a561fbe66ecef0c291971706fee4c5b6d4cbcd5d
Hamza_OK <hamza.okhadir2018@gmail.com>
2023-02-22 12:56:01 -0800

    Task Ne: 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+ * print_last_digit - a function that prints the last digit of a number.
+ * @i: the number to print his last digit
+ *
+ * Return: return the last digit of i
+ */
+int print_last_digit(int i)
+{
+	int last_digit = i % 10;
+
+	/* hundele negative number */
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
+	return (last_digit);
+}
+
