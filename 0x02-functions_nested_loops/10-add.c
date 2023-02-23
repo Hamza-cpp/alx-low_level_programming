06a1f86e3408a368ee2a5c0eb21fbdcd1681296b
Hamza_OK <hamza.okhadir2018@gmail.com>
2023-02-22 16:34:48 -0800

    Task Ne: 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that add two integers
+ * @i: first intigir
+ * @j: second intiger
+ *
+ * Return: la somme the two integers
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
+
