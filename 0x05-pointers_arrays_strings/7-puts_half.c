6d0eee23992e99787223938dbbcfd1f32d28ecd7
Hamza_OK <hamza.okhadir2018@gmail.com>
2023-02-27 15:56:31 -0800

    Task Ne: 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char to check
+ *
+ * Return: void
+ *
+ * owned by Hamza-cpp ( Hamza OKHADIR );
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i, start;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len - 1) / 2;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
